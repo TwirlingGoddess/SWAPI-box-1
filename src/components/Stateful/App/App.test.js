2c15bb9f12e7f1595cdd315217e4e39f37b6b0ea
sospinar21 <sospinar21@gmail.com>
2018-05-13 19:19:10 -0600

    Lint errors

--- src/components/Stateful/App/App.test.js
+++ src/components/Stateful/App/App.test.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import App from './App';
-import Helper from '../../Helper/Helper'
+import Helper from '../../Helper/Helper';
 import { shallow } from 'enzyme';
 import { mockPeopleApiResponse, mockPeopleObject, mockPeopleObject2, mockFavorites } from '../../../_mocks_/mockData';
-import 'jest-localstorage-mock'
+import 'jest-localstorage-mock';
 /* eslint-env mocha */
 
 describe('app', () => {
-  let app
-  let helper
+  let app;
+  let helper;
   it('matches snapshot', () => {
-    expect(app).toMatchSnapshot()
-  })
+    expect(app).toMatchSnapshot();
+  });
 
   describe('apiCall', () => {
 
     beforeEach(() => {
-      app = shallow(<App />, { disableLifecycleMethods: true })
-      helper = new Helper()
-      window.fetch = jest.fn()
-      helper.sendToLocalStorage = jest.fn()
-    })
+      app = shallow(<App />, { disableLifecycleMethods: true });
+      helper = new Helper();
+      window.fetch = jest.fn();
+      helper.sendToLocalStorage = jest.fn();
+    });
 
     it('should update the state after getting the api response', async () => {
 
@@ -29,72 +29,72 @@ describe('app', () => {
           status: 200,
           ok: true,
           json: () => Promise.resolve( mockPeopleApiResponse )
-        })
-      })
-      helper.sendToLocalStorage = jest.fn()
-      helper.makeApiCall = jest.fn()
+        });
+      });
+      helper.sendToLocalStorage = jest.fn();
+      helper.makeApiCall = jest.fn();
 
-      await app.instance().apiCall()
-      expect(app.state().loading).toEqual(false)
-      expect(app.state().selectedData).toEqual(mockPeopleObject)     
+      await app.instance().apiCall();
+      expect(app.state().loading).toEqual(false);
+      expect(app.state().selectedData).toEqual(mockPeopleObject);     
             
-    })
-  })
+    });
+  });
 
   describe('componenDidMount', () => {
     it('should have a default of empty arrays if there is no data saved in local storage', () => {
-      app.instance().componentDidMount()
-      expect(app.state().favorites).toHaveLength(0)
-    })
-  })
+      app.instance().componentDidMount();
+      expect(app.state().favorites).toHaveLength(0);
+    });
+  });
 
   describe('FindCard', () => {
-    let selectedCard    
+    let selectedCard;    
 
     it('finds the selected card and pushes it to the favorites array', () => {
-      selectedCard = {id: "people0", keyList: "people", Homeworld: "Tatooine", Population: "200000", Specie: "Human"} 
-      app.instance().findCard(selectedCard)
-      expect(app.state().favorites).toHaveLength(1)
-    })
+      selectedCard = {id: "people0", keyList: "people", Homeworld: "Tatooine", Population: "200000", Specie: "Human"}; 
+      app.instance().findCard(selectedCard);
+      expect(app.state().favorites).toHaveLength(1);
+    });
 
     it('calls addToFavorites ', () => {
-      app.instance().addCardToFavorites = jest.fn()
-      app.instance().findCard(selectedCard)
-      expect(app.instance().addCardToFavorites).toHaveBeenCalled()
-    })
-  })
+      app.instance().addCardToFavorites = jest.fn();
+      app.instance().findCard(selectedCard);
+      expect(app.instance().addCardToFavorites).toHaveBeenCalled();
+    });
+  });
 
   describe('addCardFavorites', () => {
-    let selectedCard    
+    let selectedCard;    
 
     it('checks for duplicates', () => {
-      const expectedResult = mockPeopleObject
-      app.state().favorites = mockPeopleObject
+      const expectedResult = mockPeopleObject;
+      app.state().favorites = mockPeopleObject;
     
-      selectedCard = {id: "people0", keyList: "people", Homeworld: "Tatooine", Population: "200000", Specie: "Human"} 
-      app.instance().addCardToFavorites(selectedCard)
+      selectedCard = {id: "people0", keyList: "people", Homeworld: "Tatooine", Population: "200000", Specie: "Human"}; 
+      app.instance().addCardToFavorites(selectedCard);
       expect(app.state().favorites).toEqual(expectedResult);
-    })
+    });
 
     it('adds cards to the favorites array', () => {
  
-      app.instance().addCardToFavorites(mockPeopleObject2)
-      expect(app.state().favorites).toEqual(mockPeopleObject)
-    })
-  })
+      app.instance().addCardToFavorites(mockPeopleObject2);
+      expect(app.state().favorites).toEqual(mockPeopleObject);
+    });
+  });
 
   describe('displayFavorites', () => {
-    let selectedCard    
+    let selectedCard;    
 
     it('updated the selectedData state to the favorites array', () => {
-      const expectedResult = mockPeopleObject
+      const expectedResult = mockPeopleObject;
       app.setState({
         favorites: mockPeopleObject
-      })
-      app.instance().displayFavorites()
-      expect(app.state().selectedData).toEqual(expectedResult)
+      });
+      app.instance().displayFavorites();
+      expect(app.state().selectedData).toEqual(expectedResult);
 
-    })
-  })
+    });
+  });
 
-})
+});
--- src/components/Stateful/Intro/Intro.test.js
+++ src/components/Stateful/Intro/Intro.test.js
@@ -9,7 +9,7 @@ describe('Intro', () => {
 
   beforeEach(() => {
     intro = shallow(<Intro />, { disableLifecycleMethods: true });
-    window.fetch = jest.fn()
+    window.fetch = jest.fn();
   });
   it('matches snapshot', () => {
     expect(intro).toMatchSnapshot();
@@ -23,35 +23,35 @@ describe('Intro', () => {
             status: 200,
             ok: true,
             json: () => Promise.resolve( mockPeopleApiResponse )
-          })
-        })
-      })
+          });
+        });
+      });
 
       it('calls fetch with the correct data', () => {
         const url = 'https://swapi.co/api/films';        
-        intro.instance().componentDidMount()
-        expect(window.fetch).toHaveBeenCalledWith('https://swapi.co/api/films')
-      })
+        intro.instance().componentDidMount();
+        expect(window.fetch).toHaveBeenCalledWith('https://swapi.co/api/films');
+      });
       
       describe('when the status is not ok', () => {
         it('throws an error if fetch fails', async () => {
           window.fetch.mockImplementation(() => Promise.reject(new Error('Error')));
           const expected = new Error('Error');
           await expect(intro.instance().componentDidMount()).rejects.toEqual(expected);
-        })
-      })
-    })
+        });
+      });
+    });
 
     describe('randomOpeningCrawl', () => {
 
       it('sets intro state', () => {
-        const expected = "There is unrest in the Galactic"
-        intro.instance().randomOpeningCrawl(filmCrawlResponse)
-        expect(intro.state().randomCrawl).toEqual(expected)
-        expect(intro.state().crawlTitle).toEqual('Movie Title')
+        const expected = "There is unrest in the Galactic";
+        intro.instance().randomOpeningCrawl(filmCrawlResponse);
+        expect(intro.state().randomCrawl).toEqual(expected);
+        expect(intro.state().crawlTitle).toEqual('Movie Title');
         
-      })
-    })
-  })
+      });
+    });
+  });
 
 });
